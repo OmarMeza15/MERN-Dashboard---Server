684a460dccd8c1df5adf81ace4a5157a2c77b6cd
Omar Meza <omarmezajarquin@gmail.com>
2023-08-16 23:49:35 +0200

    fixed typo in sales controller and missing object in the product stats model

--- controllers/sales.js
+++ controllers/sales.js
@@ -2,7 +2,7 @@ import OverallStat from "../models/OverallStat.js";
 
 export const getSales = async (req, res) => {
   try {
-    const overallStat = await OverallStat.fin();
+    const overallStat = await OverallStat.find();
 
     res.status(200).json(overallStat[0]);
   } catch (error) {
